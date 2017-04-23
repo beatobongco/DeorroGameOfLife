37920450bc612a79fe88a1f7a7baa5f56d9ad1a3
Beato Bongco <beatobongco@gmail.com>
2017-04-24 00:50:25 +0800

    Fix marking all near soundbar area as dead cells

--- static/js/seed.js
+++ static/js/seed.js
@@ -1,306 +1,298 @@
-var initialX = 0
-var initialY = 0
-
 function _plot(x, y, color) {
   try {
-    lifeArray[y + initialY][x + initialX].isAlive = true
+    lifeArray[y][x].isAlive = true
   } catch (err) {
     //pass
   }
 }
 
 function seedLife() {
+  function rplot(x, y) {
+    _plot(x + 17, y + 7)
+  }
   //D
-  _plot(1, 1)
-  _plot(2, 1)
-  _plot(3, 1)
+  rplot(1, 1)
+  rplot(2, 1)
+  rplot(3, 1)
 
-  _plot(1, 2)
-  _plot(4, 2)
+  rplot(1, 2)
+  rplot(4, 2)
 
-  _plot(1, 3)
-  _plot(4, 3)
+  rplot(1, 3)
+  rplot(4, 3)
 
-  _plot(1, 4)
-  _plot(4, 4)
+  rplot(1, 4)
+  rplot(4, 4)
 
-  _plot(1, 5)
-  _plot(2, 5)
-  _plot(3, 5)
+  rplot(1, 5)
+  rplot(2, 5)
+  rplot(3, 5)
 
   //E
-  _plot(6, 1)
-  _plot(7, 1)
-  _plot(8, 1)
-  _plot(9, 1)
+  rplot(6, 1)
+  rplot(7, 1)
+  rplot(8, 1)
+  rplot(9, 1)
 
-  _plot(6, 2)
+  rplot(6, 2)
 
-  _plot(6, 3)
-  _plot(7, 3)
-  _plot(8, 3)
+  rplot(6, 3)
+  rplot(7, 3)
+  rplot(8, 3)
 
-  _plot(6, 4)
+  rplot(6, 4)
 
-  _plot(6, 5)
-  _plot(7, 5)
-  _plot(8, 5)
-  _plot(9, 5)
-
-  //O
-  _plot(12, 1)
-  _plot(13, 1)
-
-  _plot(11, 2)
-  _plot(11, 3)
-  _plot(11, 4)
-
-  _plot(14, 2)
-  _plot(14, 3)
-  _plot(14, 4)
-
-  _plot(12, 5)
-  _plot(13, 5)
-
-  //R
-  _plot(16, 1)
-  _plot(17, 1)
-  _plot(18, 1)
-
-  _plot(16, 2)
-  _plot(19, 2)
-
-  _plot(16, 3)
-  _plot(17, 3)
-  _plot(18, 3)
-  _plot(19, 3)
-
-  _plot(16, 4)
-  _plot(18, 4)
-
-  _plot(16, 5)
-  _plot(19, 5)
-
-  //R
-  _plot(21, 1)
-  _plot(22, 1)
-  _plot(23, 1)
-
-  _plot(21, 2)
-  _plot(24, 2)
-
-  _plot(21, 3)
-  _plot(22, 3)
-  _plot(23, 3)
-  _plot(24, 3)
-
-  _plot(21, 4)
-  _plot(23, 4)
-
-  _plot(21, 5)
-  _plot(24, 5)
+  rplot(6, 5)
+  rplot(7, 5)
+  rplot(8, 5)
+  rplot(9, 5)
 
   //O
-  _plot(27, 1)
-  _plot(28, 1)
+  rplot(12, 1)
+  rplot(13, 1)
 
-  _plot(26, 2)
-  _plot(26, 3)
-  _plot(26, 4)
+  rplot(11, 2)
+  rplot(11, 3)
+  rplot(11, 4)
 
-  _plot(29, 2)
-  _plot(29, 3)
-  _plot(29, 4)
+  rplot(14, 2)
+  rplot(14, 3)
+  rplot(14, 4)
 
-  _plot(27, 5)
-  _plot(28, 5)
+  rplot(12, 5)
+  rplot(13, 5)
+
+  //R
+  rplot(16, 1)
+  rplot(17, 1)
+  rplot(18, 1)
+
+  rplot(16, 2)
+  rplot(19, 2)
+
+  rplot(16, 3)
+  rplot(17, 3)
+  rplot(18, 3)
+  rplot(19, 3)
+
+  rplot(16, 4)
+  rplot(18, 4)
+
+  rplot(16, 5)
+  rplot(19, 5)
+
+  //R
+  rplot(21, 1)
+  rplot(22, 1)
+  rplot(23, 1)
+
+  rplot(21, 2)
+  rplot(24, 2)
+
+  rplot(21, 3)
+  rplot(22, 3)
+  rplot(23, 3)
+  rplot(24, 3)
+
+  rplot(21, 4)
+  rplot(23, 4)
+
+  rplot(21, 5)
+  rplot(24, 5)
+
+  //O
+  rplot(27, 1)
+  rplot(28, 1)
+
+  rplot(26, 2)
+  rplot(26, 3)
+  rplot(26, 4)
+
+  rplot(29, 2)
+  rplot(29, 3)
+  rplot(29, 4)
+
+  rplot(27, 5)
+  rplot(28, 5)
 
   //T
-  _plot(7, 8)
-  _plot(8, 8)
-  _plot(9, 8)
+  rplot(12, 8)
+  rplot(13, 8)
+  rplot(14, 8)
 
-  _plot(8, 9)
-  _plot(8, 10)
-  _plot(8, 11)
-  _plot(8, 12)
+  rplot(13, 9)
+  rplot(13, 10)
+  rplot(13, 11)
+  rplot(13, 12)
 
   //U
-  _plot(11, 8)
-  _plot(11, 9)
-  _plot(11, 10)
-  _plot(11, 11)
+  rplot(16, 8)
+  rplot(16, 9)
+  rplot(16, 10)
+  rplot(16, 11)
 
-  _plot(12, 12)
-  _plot(13, 12)
-  _plot(14, 12)
-  _plot(14, 11)
-  _plot(14, 10)
-  _plot(14, 9)
-  _plot(14, 8)
+  rplot(17, 12)
+  rplot(18, 12)
+  rplot(19, 12)
+  rplot(19, 11)
+  rplot(19, 10)
+  rplot(19, 9)
+  rplot(19, 8)
 
   //R
-  _plot(16, 8)
-  _plot(17, 8)
-  _plot(18, 8)
+  rplot(21, 8)
+  rplot(22, 8)
+  rplot(23, 8)
 
-  _plot(16, 9)
-  _plot(19, 9)
+  rplot(21, 9)
+  rplot(24, 9)
 
-  _plot(16, 10)
-  _plot(17, 10)
-  _plot(18, 10)
-  _plot(19, 10)
+  rplot(21, 10)
+  rplot(22, 10)
+  rplot(23, 10)
+  rplot(24, 10)
 
-  _plot(16, 11)
-  _plot(18, 11)
+  rplot(21, 11)
+  rplot(23, 11)
 
-  _plot(16, 12)
-  _plot(19, 12)
+  rplot(21, 12)
+  rplot(24, 12)
 
   //N
-  _plot(21, 8)
-  _plot(21, 9)
-  _plot(21, 10)
-  _plot(21, 11)
-  _plot(21, 12)
+  rplot(26, 8)
+  rplot(26, 9)
+  rplot(26, 10)
+  rplot(26, 11)
+  rplot(26, 12)
 
-  _plot(22, 9)
-  _plot(23, 10)
+  rplot(27, 9)
+  rplot(28, 10)
 
-  _plot(24, 8)
-  _plot(24, 9)
-  _plot(24, 10)
-  _plot(24, 11)
-  _plot(24, 12)
+  rplot(29, 8)
+  rplot(29, 9)
+  rplot(29, 10)
+  rplot(29, 11)
+  rplot(29, 12)
 
   //B
-  _plot(11, 15)
-  _plot(12, 15)
+  rplot(11, 15)
+  rplot(12, 15)
 
-  _plot(11, 16)
-  _plot(13, 16)
+  rplot(11, 16)
+  rplot(13, 16)
 
-  _plot(11, 17)
-  _plot(12, 17)
-  _plot(13, 17)
-  _plot(14, 17)
+  rplot(11, 17)
+  rplot(12, 17)
+  rplot(13, 17)
+  rplot(14, 17)
 
-  _plot(11, 18)
-  _plot(14, 18)
+  rplot(11, 18)
+  rplot(14, 18)
 
-  _plot(11, 19)
-  _plot(12, 19)
-  _plot(13, 19)
+  rplot(11, 19)
+  rplot(12, 19)
+  rplot(13, 19)
 
   //A
-  _plot(17, 15)
-  _plot(18, 15)
-  _plot(16, 16)
-  _plot(19, 16)
-  _plot(16, 17)
-  _plot(19, 17)
+  rplot(17, 15)
+  rplot(18, 15)
+  rplot(16, 16)
+  rplot(19, 16)
+  rplot(16, 17)
+  rplot(19, 17)
 
-  _plot(16, 18)
-  _plot(17, 18)
-  _plot(18, 18)
-  _plot(19, 18)
+  rplot(16, 18)
+  rplot(17, 18)
+  rplot(18, 18)
+  rplot(19, 18)
 
-  _plot(16, 19)
-  _plot(19, 19)
+  rplot(16, 19)
+  rplot(19, 19)
 
   //C
-  _plot(21, 16)
-  _plot(21, 17)
-  _plot(21, 18)
+  rplot(21, 16)
+  rplot(21, 17)
+  rplot(21, 18)
 
-  _plot(22, 15)
-  _plot(23, 15)
-  _plot(24, 15)
+  rplot(22, 15)
+  rplot(23, 15)
+  rplot(24, 15)
 
-  _plot(22, 19)
-  _plot(23, 19)
-  _plot(24, 19)
+  rplot(22, 19)
+  rplot(23, 19)
+  rplot(24, 19)
 
   //K
-  _plot(26, 15)
-  _plot(26, 16)
-  _plot(26, 17)
-  _plot(26, 18)
-  _plot(26, 19)
+  rplot(26, 15)
+  rplot(26, 16)
+  rplot(26, 17)
+  rplot(26, 18)
+  rplot(26, 19)
 
-  _plot(27, 17)
-  _plot(28, 16)
-  _plot(28, 18)
-  _plot(29, 15)
-  _plot(29, 19)
+  rplot(27, 17)
+  rplot(28, 16)
+  rplot(28, 18)
+  rplot(29, 15)
+  rplot(29, 19)
 
   //T
-  _plot(22, 22)
-  _plot(23, 22)
-  _plot(24, 22)
+  rplot(12, 22)
+  rplot(13, 22)
+  rplot(14, 22)
 
-  _plot(23, 23)
-  _plot(23, 24)
-  _plot(23, 25)
-  _plot(23, 26)
+  rplot(13, 23)
+  rplot(13, 24)
+  rplot(13, 25)
+  rplot(13, 26)
 
   //I
-  _plot(26, 22)
-  _plot(27, 22)
-  _plot(28, 22)
+  rplot(16, 22)
+  rplot(17, 22)
+  rplot(18, 22)
 
-  _plot(27, 23)
-  _plot(27, 24)
-  _plot(27, 25)
-  _plot(27, 26)
+  rplot(17, 23)
+  rplot(17, 24)
+  rplot(17, 25)
+  rplot(17, 26)
 
-  _plot(26, 26)
-  _plot(28, 26)
+  rplot(16, 26)
+  rplot(18, 26)
 
   //M
-  _plot(30, 22)
-  _plot(30, 23)
-  _plot(30, 24)
-  _plot(30, 25)
-  _plot(30, 26)
+  rplot(20, 22)
+  rplot(20, 23)
+  rplot(20, 24)
+  rplot(20, 25)
+  rplot(20, 26)
 
-  _plot(31, 23)
-  _plot(32, 24)
-  _plot(33, 23)
+  rplot(21, 23)
+  rplot(22, 24)
+  rplot(23, 23)
 
-  _plot(34, 22)
-  _plot(34, 23)
-  _plot(34, 24)
-  _plot(34, 25)
-  _plot(34, 26)
+  rplot(24, 22)
+  rplot(24, 23)
+  rplot(24, 24)
+  rplot(24, 25)
+  rplot(24, 26)
 
   //E
-  _plot(36, 22)
-  _plot(37, 22)
-  _plot(38, 22)
-  _plot(39, 22)
+  rplot(26, 22)
+  rplot(27, 22)
+  rplot(28, 22)
+  rplot(29, 22)
 
-  _plot(36, 23)
+  rplot(26, 23)
 
-  _plot(36, 24)
-  _plot(37, 24)
-  _plot(38, 24)
+  rplot(26, 24)
+  rplot(27, 24)
+  rplot(28, 24)
 
-  _plot(36, 25)
+  rplot(26, 25)
 
-  _plot(36, 26)
-  _plot(37, 26)
-  _plot(38, 26)
-  _plot(39, 26)
-}
-
-function generateGlider(x, y) {
-  _plot(x + 1, y)
-  _plot(x + 2, y + 1)
-  _plot(x, y + 2)
-  _plot(x + 1, y + 2)
-  _plot(x + 2, y + 2)
+  rplot(26, 26)
+  rplot(27, 26)
+  rplot(28, 26)
+  rplot(29, 26)
 }
 
 function generateUpGlider(x, y) {
@@ -309,9 +301,79 @@ function generateUpGlider(x, y) {
   _plot(x + 1, y)
   _plot(x + 2, y)
   _plot(x + 1, y + 2)
-  if (random() < .5) {
-    _plot(x + 2, y + 1)
-  } else {
-    _plot(x, y + 1)
+  _plot(x + 2, y + 1)
+
+  // if (random() < .5) {
+  //   _plot(x + 2, y + 1)
+  // } else {
+  //   _plot(x, y + 1)
+  // }
+}
+
+function generatePulsar(x, y) {
+  function rplot(i, j) {
+    _plot(x + i, y + j)
   }
+  rplot(2, 0)
+  rplot(3, 0)
+  rplot(4, 0)
+
+  rplot(8, 0)
+  rplot(9, 0)
+  rplot(10, 0)
+
+  rplot(0, 2)
+  rplot(5, 2)
+  rplot(7, 2)
+  rplot(12, 2)
+
+  rplot(0, 3)
+  rplot(5, 3)
+  rplot(7, 3)
+  rplot(12, 3)
+
+  rplot(0, 4)
+  rplot(5, 4)
+  rplot(7, 4)
+  rplot(12, 4)
+
+  rplot(2, 5)
+  rplot(3, 5)
+  rplot(4, 5)
+
+  rplot(8, 5)
+  rplot(9, 5)
+  rplot(10, 5)
+
+  rplot(2, 7)
+  rplot(3, 7)
+  rplot(4, 7)
+
+  rplot(8, 7)
+  rplot(9, 7)
+  rplot(10, 7)
+
+  rplot(0, 8)
+  rplot(5, 8)
+  rplot(7, 8)
+  rplot(12, 8)
+
+  rplot(0, 9)
+  rplot(5, 9)
+  rplot(7, 9)
+  rplot(12, 9)
+
+  rplot(0, 10)
+  rplot(5, 10)
+  rplot(7, 10)
+  rplot(12, 10)
+
+  rplot(2, 12)
+  rplot(3, 12)
+  rplot(4, 12)
+
+  rplot(8, 12)
+  rplot(9, 12)
+  rplot(10, 12)
+
 }
